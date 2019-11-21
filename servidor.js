448379f69b72b4b7068c4f6ecf68ac33824ab197
jordi2000 <55458664+jordi2000@users.noreply.github.com>
2019-11-21 15:00:46 -0600

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var url = require('url');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('HOLA!, este es el servidor de <b>Jordi Amador</b>');
+  var q = url.parse(req.url, true).query;
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('HOLA' + q.nombre);
 }).listen(process.env.PORT);
