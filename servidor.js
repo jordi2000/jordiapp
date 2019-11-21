1480c8765f816b808d99e2f3fe7f252c3c2f7e1a
jordi2000 <55458664+jordi2000@users.noreply.github.com>
2019-11-21 15:52:16 -0600

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -11,8 +11,26 @@ http.createServer(function (req, res) {
   if (typeof jugador.nombre === 'string' &&
      typeof jugador.pos === 'object' &&
      typeof jugador.pos.length === 'number' &&
-     jugador.pos.length === 2) {
-    res.end('correcto');
+     jugador.pos.length === 2 &&
+     typeof jugador.tam === 'number') {
+    //buscar al objeto
+    var indice = -1;
+    for (var i = 0; i < juagdores.length; i++) {
+    if(jugadores[i].nom === jugador.nom;) {
+   indice = i;
+     break;
+    }
+    }
+    if (indice < 0) {
+      //crear
+      jugadores.push(jugador);
+      res.end('jugador creado');
+    }else{
+      //actualizar
+      jugadores[indice] = jugador;
+      res.end('jugador actualizado');
+    }
+    
   } else {
     res.end('incorrecto');
   }
