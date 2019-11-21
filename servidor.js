b039ba28aab8136d8d200dd058c244af7eb2bbc8
jordi2000 <55458664+jordi2000@users.noreply.github.com>
2019-11-21 15:19:18 -0600

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -6,8 +6,8 @@ var jugadores = [];
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   var q = url.parse(req.url, true).query;
-  if (JSON.parse(q.jugador)) {
-  res.end('interpretado');
+  if (q.jugador) {
+  res.end('Recibido');
   } else {
     res.end('error');
   }
