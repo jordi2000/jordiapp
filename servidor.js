8aeb1f206b30dde88b5331da709077c7126ff7ca
jordi2000 <55458664+jordi2000@users.noreply.github.com>
2019-11-21 15:17:03 -0600

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,11 +1,16 @@
 var http = require('http');
 var url = require('url');
 
-var nombres = [];
+var jugadores = [];
 
 http.createServer(function (req, res) {
-  var q = url.parse(req.url, true).query;
-  nombres.push(q.nombre);
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Lista De Nombres ' + nombres.join());
+  var q = url.parse(req.url, true).query;
+  if (JSON.parse(q.jugador)) {
+  res.end('interpretado');
+  } else {
+    res.end('error');
+  }
+  
+  
 }).listen(process.env.PORT);
